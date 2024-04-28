d7334760b236fd11fc65e72e214e1ac1e78fce8d
koctt <147679458+koctt-10@users.noreply.github.com>
2024-04-28 09:48:17 +0800

    header parser
    pip install request

---
+++ header_parser.py
@@ -0,0 +1,8 @@
+import requests
+i = input("Link")
+response = requests.get(i)
+items = response.headers.items()
+headers = [f'{key}: {header}' for key, header in items]
+formatted_headers = '\n'.join(headers)
+with open('headers.txt', 'w') as file:
+    file.write(formatted_headers)
